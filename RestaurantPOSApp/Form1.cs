593c1fff1fcf6bff77b4a8fb83394f8e014f0454
Khaled A <almasri.k@hotmail.com>
2018-11-10 16:33:29 -0500

    Test Push 1

--- RestaurantPOSApp/Form1.cs
+++ RestaurantPOSApp/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RestaurantPOSApp
 {
-    public partial class Form1 : Form
+    public partial class Form1_rename : Form
     {
-        public Form1()
+        public Form1_rename()
         {
             InitializeComponent();
         }
