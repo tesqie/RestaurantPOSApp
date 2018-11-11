0fe11d13e1ac7dd863f97d270a26b41868e407e6
Abdul Tesqie <tesqiebay@gmail.com>
2018-11-10 21:22:14 -0500

    testing

--- RestaurantPOSApp/Form2.Designer.cs
+++ RestaurantPOSApp/Form2.Designer.cs
@@ -29,11 +29,9 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-
             this.button2 = new System.Windows.Forms.Button();
-
             this.textBox1 = new System.Windows.Forms.TextBox();
-
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // button1
@@ -45,7 +43,6 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(227, 193);
@@ -54,24 +51,30 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-
+            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(397, 159);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
-
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(209, 266);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 2;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
-
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -87,5 +90,6 @@
         private System.Windows.Forms.Button button2;
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
